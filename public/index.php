099ce0947e1a9af14b109c9fc6a807a3a5ecf91f
sunlazor <fakemailexample@mail.cc>
2024-05-20 00:18:51 +0300

    symfony 7 installation

--- public/index.php
+++ public/index.php
@@ -1,19 +1,9 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+use App\Kernel;
 
-// initialize the Application kernel
-$app = new \Runner\Application();
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-// create Request object
-$worker = \Spiral\RoadRunner\Worker::create();
-$psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
-$creator = new \Spiral\RoadRunner\Http\PSR7Worker($worker, $psr17Factory, $psr17Factory, $psr17Factory);
-
-while ($request = $creator->waitRequest()) {
-    // pass the Request object to the Application kernel
-    $response = $app->handle($request);
-
-    // emit Response
-    $creator->respond($response);
-}
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
