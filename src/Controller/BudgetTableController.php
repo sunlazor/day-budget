175b5d57c0b1f33b6da4c53b2e8ac146e4322c9c
sunlazor <fakemailexample@mail.cc>
2024-06-18 22:29:51 +0300

    temp save

--- src/Controller/BudgetTableController.php
+++ src/Controller/BudgetTableController.php
@@ -6,15 +6,28 @@ namespace App\Controller;
 
 use App\Form\BudgetTableForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BudgetTableController extends AbstractController
 {
     #[Route('/budget')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $form = $this->createForm(BudgetTableForm::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+//            dump($request);
+//            $form
+//                ->add('title', TextType::class, ['required' => false])
+//            ;
+
+            return $this->redirectToRoute('');
+        }
         return $this->render(
             'budget/base.html.twig',
             ['form' => $form->createView()],
--- src/Form/BudgetTableForm.php
+++ src/Form/BudgetTableForm.php
@@ -5,16 +5,21 @@ declare(strict_types=1);
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BudgetTableForm extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
+            ->add('title', TextType::class, ['required' => false])
+            ->add('addMore', SubmitType::class)
+//            ->add('addMore', SubmitType::class)
+//            ->addEventListener('addMorePressed', fn () => $builder->add('title', TextType::class, ['required' => false]))
         ;
     }
 }
